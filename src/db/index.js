5b01a23e1f70beef6f3301f3a5ab71f984f27825
Ashish Kumar <akgautam1603@gmail.com>
2025-11-19 01:44:06 +0530

    add comments

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,7 @@ import {DB_NAME} from "../constants.js";
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`) //Async isliye, kyunki DB connect hone me time lagta hai.Without async/await, code aage badh jaata aur server crash hota.
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("MONGODB connection error: ", error)
@@ -14,3 +14,21 @@ const connectDB = async () => {
 }
 
 export default connectDB
+
+/* Is DB connect function me kaise connection hota hai — Summary:
+
+Function call hota → connectDB()
+
+mongoose.connect() chalti hai
+
+Connection string parse hoti
+
+DNS resolve hota
+
+Mongo server se TCP + SSL handshake hota
+
+Auth check hoti
+
+Connection pool banata
+
+Success return hota → connectionInstance */
--- src/index.js
+++ src/index.js
@@ -16,24 +16,29 @@ connectDB()
 
 
 /* import express from "express"
-const app = express()
+const app = express() //Yeh object tumhari puri application ko represent karta hai.Isko hi later port pe start karoge.
+
 
 ( async () => {
     try {
-        await mongoose.connect(`${process.env.MOBGODB_URI}/${DB_NAME}`)// db ko connect kiya
+        await mongoose.connect(`${process.env.MOBGODB_URI}/${DB_NAME}`)// await lagaya so that:Execution yahin ruk jayeJab tak database connect nahi hota, server आगे ना बढे
+
         app.on("error", (error) => {
             console.log("ERRR: ", error)
             throw error
-        })// port ko listen krta hai
+        }) //Ye event listener express app ke internal errors ke liye hai.Agar server while running koi internal error throw kare → yeh chalega.
 
         app.listen(process.env.PORT, () => {
             console.log(`App is listening on port ${process.env.PORT}`)
-        })
+        })//Server ko start kar raha specific port par
 
     } catch (error) {
         console.error("ERROR: ", error)
         throw err
         
-    }
+    } // try-catch: Iska purpose: agar DB connection fail ho jaye, to server crash na ho silently.
 
-})() */
+})() //Yeh ek Immediately Invoked Async Function Expression (IIFE) hai.
+// Iske andar await use kar sakte ho (top-level pe nahi hota older Node versions me)
+
+Yeh function turant execute ho jata hai — bina kisi manual function call ke */
